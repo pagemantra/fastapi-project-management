bfb0aff77c3b20891883e765ceabf0b134331a9e
Pavankumar Gollapudi <pagemantra@gmail.com>
2025-12-09 17:42:30 +0530

    fix

--- models/__init__.py
+++
@@ -1,3 +0,0 @@
-from models.user import UserRole, UserBase, UserCreate, UserUpdate, UserInDB, UserResponse
-from models.task import TaskStatus, TaskPriority, TaskBase, TaskCreate, TaskUpdate, TaskInDB, TaskResponse
-from models.team import TeamBase, TeamCreate, TeamUpdate, TeamInDB, TeamResponse
--- routes/__init__.py
+++
@@ -1,4 +0,0 @@
-from routes.auth import router as auth_router
-from routes.users import router as users_router
-from routes.teams import router as teams_router
-from routes.tasks import router as tasks_router
